2654fdc5792d482fb425d25c8ed1c41318989d83
ASHIK-KHAN-ATUL <ashikkhan693693@gmail.com>
2025-09-19 18:59:16 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,31 @@
+// Basic data type
+
+// String
+let firstName: string = "Ashik";
+
+// Number
+let roll: number = 533084;
+
+// boolean
+let admin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+// Array
+let friends: string[] = ["Ashik", "Faim", "Akib"];
+
+let eligibleRollList: number[] = [1, 2, 3, 5, 6, 8];
+eligibleRollList.push(2);
+
+// tuple --> order --> type of values
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [22, "Ashik", true];
+ageName[0] = 15;
---
+++ module1/src/1.5.ts
@@ -0,0 +1,13 @@
+const user: {
+  company: "Programing Hero";
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "Programing Hero",
+  firstName: "Ashik",
+  middleName: "Khan",
+  lastName: "Atul",
+  isMarried: true,
+};
